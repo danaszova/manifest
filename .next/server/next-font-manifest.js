bcc78bf71e9536d768d4c0a1761bd5f29d38d94e
Dana Szova <danaszova@Danas-MBP.attlocal.net>
2025-01-11 12:46:48 -0800

    Remove .next folder from tracking

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/danaszova/Storage/DANASZOVA/ALPHA/manifest/group-gear/app/layout\":[\"static/media/a34f9d1faa5f3315-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
